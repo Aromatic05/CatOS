d443eab57a2928f307284c7079291e069cb8694d
Aromatic <symwww@outlook.com>
2025-09-15 12:43:22 +0800

    add nvidia modules

--- catos-iso/profiledef.sh
+++ catos-iso/profiledef.sh
@@ -14,7 +14,7 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 airootfs_image_tool_options=('-comp' 'zstd')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
@@ -23,4 +23,5 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/usr/local/bin/nvidia-module-loader"]="0:0:755"
 )
