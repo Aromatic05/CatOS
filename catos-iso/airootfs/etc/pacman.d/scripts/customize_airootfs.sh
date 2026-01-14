6e88639b57537296666533bc61b0e9dbbaae27cd
Aromatic <symwww@outlook.com>
2026-01-14 14:01:23 +0800

    fix

--- catos-iso/airootfs/etc/pacman.d/scripts/customize_airootfs.sh
+++ catos-iso/airootfs/etc/pacman.d/scripts/customize_airootfs.sh
@@ -9,18 +9,9 @@ set -e -u
 # echo 'SigLevel = Never' >> /etc/pacman.conf
 # echo 'Server = https://pkgs.catos.info/$arch' >> /etc/pacman.conf
 
-#remove kde welcome
-rm /etc/xdg/autostart/calamares.desktop
-
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-
-mkdir -p /home/liveuser/Desktop
-mv /etc/xdg/autostart/catos.desktop /home/liveuser/Desktop/catos.desktop
-#rm /etc/xdg/autostart/catos-advanced.desktop  #暂时移除联网安装
-mv /etc/xdg/autostart/catos-advanced.desktop /home/liveuser/Desktop/catos-advanced.desktop
-
 
 ##grub
 #echo 'GRUB_THEME="/usr/share/grub/themes/vimix-color-1080p/theme.txt"' >> /etc/default/grub
